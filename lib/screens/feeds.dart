57a5f81dc977f763a6723858f62a34901d1e8ff7
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-27 12:22:56 +0000

    Implemented feeds

---
+++ lib/screens/feeds.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Feeds extends StatelessWidget {
+  const Feeds({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Hello'),),
+    );
+  }
+}
