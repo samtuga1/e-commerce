cf832381307a9c72a0f0480d0e8b535f7b966147
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2022-05-27 12:23:33 +0000

    implemented the user info

---
+++ lib/screens/user_info.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UserInfo extends StatelessWidget {
+  const UserInfo({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Hello'),),
+    );
+  }
+}
