93de7b9c26b018d3302a25e08be402e3b2a73ea1
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-08 23:54:02 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:e_commerse/consts/theme_data.dart';
+import 'package:e_commerse/models/dark_theme_sharedpreference.dart';
+import 'package:e_commerse/providers/theme_provider.dart';
 import 'package:e_commerse/screens/user_info.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'screens/bottom_bar.dart';
 
@@ -7,16 +11,43 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  DarkThemeProvider _themeProvider = DarkThemeProvider();
+
+  void getCurrentTheme() async {
+    _themeProvider.darkTheme = await _themeProvider.darkThemePrefs.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentTheme();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: BottomBarScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => _themeProvider,
+        ),
+      ],
+      child: Consumer<DarkThemeProvider>(builder: (context, themeData, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: Styles.themeData(themeData.darkTheme, context),
+          home: BottomBarScreen(),
+          routes: {
+            UserInfo.routeName: (context) => UserInfo(),
+          },
+        );
+      }),
     );
   }
 }
