f1472dd6cdbc4d874c00f8edf4960564d388ef42
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-09 15:41:21 +0000

    update

--- lib/screens/feeds.dart
+++ lib/screens/feeds.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerse/widgets/feeds_products.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class Feeds extends StatelessWidget {
   const Feeds({Key? key}) : super(key: key);
@@ -7,7 +8,17 @@ class Feeds extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FeedProducts(),
-    );
+        body: SafeArea(
+      child: MasonryGridView.count(
+        padding: EdgeInsets.all(3),
+        crossAxisCount: 2,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        itemCount: 10,
+        itemBuilder: (context, index) => FeedProducts(
+          height: index % 2 == 0 ? 310 : 270,
+        ),
+      ),
+    ));
   }
 }
