72fdd1968e01788013173b5f34c54d3e688e5479
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-10 09:53:08 +0000

    update

---
+++ lib/inner_screens/brands_rail_widget.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class BrandsNavigationRail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        color: Colors.red,
+        padding: EdgeInsets.only(left: 5.0, right: 5.0),
+        margin: EdgeInsets.only(right: 20.0, bottom: 5, top: 18),
+        constraints: BoxConstraints(
+            minHeight: 150, minWidth: double.infinity, maxHeight: 180),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).backgroundColor,
+                  image: DecorationImage(
+                      image: NetworkImage(
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4PdHtXka2-bDDww6Nuect3Mt9IwpE4v4HNw&usqp=CAU',
+                      ),
+                      fit: BoxFit.contain),
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 2.0)
+                  ],
+                ),
+              ),
+            ),
+            FittedBox(
+              child: Container(
+                margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                decoration: BoxDecoration(
+                    color: Theme.of(context).backgroundColor,
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(10.0),
+                        topRight: Radius.circular(10.0)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(5.0, 5.0),
+                          blurRadius: 10.0)
+                    ]),
+                width: 160,
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'title',
+                      maxLines: 4,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          color: Theme.of(context).textSelectionColor),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    FittedBox(
+                      child: Text('US 16 \$',
+                          maxLines: 1,
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 30.0,
+                          )),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Text('CatergoryName',
+                        style: TextStyle(color: Colors.grey, fontSize: 18.0)),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
