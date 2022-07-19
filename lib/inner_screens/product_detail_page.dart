3047d560ebe849286732a5767e26d11da64109fc
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-19 02:25:01 +0000

    update

---
+++ lib/inner_screens/product_detail_page.dart
@@ -0,0 +1,377 @@
+import 'package:e_commerse/consts/colors.dart';
+import 'package:e_commerse/providers/theme_provider.dart';
+import 'package:e_commerse/screens/wishlist.dart';
+import 'package:e_commerse/widgets/feeds_products.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+import '../screens/cart.dart';
+
+class ProductDetailPage extends StatefulWidget {
+  static const routeName = '/productsDetails';
+  const ProductDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<ProductDetailPage> createState() => _ProductDetailPageState();
+}
+
+class _ProductDetailPageState extends State<ProductDetailPage> {
+  GlobalKey previewContainer = new GlobalKey();
+  Widget _details(bool themeState, String title, String info) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15.0, left: 16, right: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color: Theme.of(context).textSelectionColor,
+              fontWeight: FontWeight.w600,
+              fontSize: 21.0,
+            ),
+          ),
+          Text(
+            info,
+            style: TextStyle(
+              color: themeState
+                  ? Theme.of(context).disabledColor
+                  : ColorsConsts.subTitle,
+              fontWeight: FontWeight.w100,
+              fontSize: 20.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            foregroundDecoration: BoxDecoration(
+              color: Colors.black12,
+            ),
+            height: MediaQuery.of(context).size.height * 0.45,
+            width: double.infinity,
+            child: Image.network(
+                'https://images.pexels.com/photos/5812887/pexels-photo-5812887.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+          ),
+          SingleChildScrollView(
+            padding: const EdgeInsets.only(top: 36, bottom: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 250,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          splashColor: Colors.purple.shade200,
+                          onTap: () {},
+                          borderRadius: BorderRadius.circular(30),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Icon(
+                              Icons.save,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          splashColor: Colors.purple.shade200,
+                          onTap: () {},
+                          borderRadius: BorderRadius.circular(30),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Icon(
+                              Icons.share,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.9,
+                              child: Text(
+                                'title',
+                                maxLines: 2,
+                                style: TextStyle(
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              'US \$ 15',
+                              style: TextStyle(
+                                fontSize: 21,
+                                color: themeState.darkTheme
+                                    ? Theme.of(context).disabledColor
+                                    : ColorsConsts.subTitle,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 3.0,
+                      ),
+                      Divider(
+                        thickness: 1,
+                        color: Colors.grey,
+                        height: 1,
+                        indent: 8,
+                        endIndent: 8,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'US \$ 15',
+                          style: TextStyle(
+                            fontSize: 21,
+                            color: themeState.darkTheme
+                                ? Theme.of(context).disabledColor
+                                : ColorsConsts.subTitle,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5.0,
+                      ),
+                      Divider(
+                        thickness: 1,
+                        color: Colors.grey,
+                        height: 1,
+                        indent: 8,
+                        endIndent: 8,
+                      ),
+                      _details(themeState.darkTheme, 'Brand', 'BrandName'),
+                      _details(themeState.darkTheme, 'Quantity', '12 Left'),
+                      _details(themeState.darkTheme, 'Category', 'Cat Name'),
+                      _details(themeState.darkTheme, 'Popularity', 'Popular'),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Divider(
+                        thickness: 1,
+                        color: Colors.grey,
+                        height: 1,
+                      ),
+                      Container(
+                        color: Theme.of(context).backgroundColor,
+                        width: double.infinity,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const SizedBox(
+                              height: 10.0,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text(
+                                'Be the first review',
+                                style: TextStyle(
+                                  color: themeState.darkTheme
+                                      ? Theme.of(context).disabledColor
+                                      : ColorsConsts.subTitle,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 50,
+                            ),
+                            Divider(
+                              thickness: 1,
+                              color: Colors.grey,
+                              height: 1,
+                            ),
+                            Container(
+                              width: double.infinity,
+                              padding: const EdgeInsets.all(8.0),
+                              color: Theme.of(context).scaffoldBackgroundColor,
+                              child: Text(
+                                'Suggested products',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: double.infinity,
+                              margin: const EdgeInsets.only(bottom: 30.0),
+                              height: 300,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: 7,
+                                itemBuilder: (context, index) =>
+                                    FeedProducts(height: 80),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: true,
+              title: Text(
+                'DETAIL',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(WishlistScreen.routeName);
+                  },
+                  icon: Icon(
+                    Icons.favorite,
+                    color: ColorsConsts.favColor,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(CartScreen.routeName);
+                  },
+                  icon: Icon(
+                    Icons.shopping_cart,
+                    color: ColorsConsts.cartColor,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    height: 50,
+                    child: RaisedButton(
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      shape: RoundedRectangleBorder(side: BorderSide.none),
+                      color: Colors.redAccent.shade100,
+                      onPressed: () {},
+                      child: Text(
+                        'ADD TO CART',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 50,
+                    child: RaisedButton(
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      shape: RoundedRectangleBorder(side: BorderSide.none),
+                      color: Colors.redAccent.shade100,
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Text(
+                            'BUY NOW',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Icon(
+                            Icons.payment,
+                            color: Colors.green.shade700,
+                            size: 19,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 50,
+                    color: themeState.darkTheme
+                        ? Theme.of(context).disabledColor
+                        : ColorsConsts.subTitle,
+                    child: InkWell(
+                      splashColor: ColorsConsts.favColor,
+                      onTap: () {},
+                      child: Center(
+                        child: Icon(
+                          Icons.favorite_outline,
+                          color: ColorsConsts.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
