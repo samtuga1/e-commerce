cf2d6444f6aee8bbad2d8a38aa50d21234233247
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-19 00:43:57 +0000

    update

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -2,88 +2,18 @@ import 'package:e_commerse/widgets/cart_empty.dart';
 import 'package:e_commerse/widgets/cart_full.dart';
 import 'package:flutter/material.dart';
 
-import '../consts/colors.dart';
-
 class Cart extends StatelessWidget {
   const Cart({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Widget checkoutSection(BuildContext ctx) {
-      return Container(
-        decoration: BoxDecoration(
-          border: Border(
-            top: BorderSide(
-              width: 0.5,
-              color: Colors.grey,
-            ),
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16.0),
-                    gradient: LinearGradient(colors: [
-                      ColorsConsts.gradiendLStart,
-                      ColorsConsts.gradiendLEnd,
-                    ], stops: [
-                      0.0,
-                      0.7
-                    ]),
-                  ),
-                  child: Material(
-                    color: Colors.transparent,
-                    child: InkWell(
-                      onTap: () {},
-                      borderRadius: BorderRadius.circular(16.0),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Checkout',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Theme.of(ctx).textSelectionColor,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const Spacer(),
-              Text(
-                'Total:  ',
-                style: TextStyle(
-                  color: Theme.of(ctx).textSelectionColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              Text(
-                'US\$400.00',
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-
     List products = [2];
-    return Scaffold(
-      appBar: products.isNotEmpty
-          ? AppBar(
+    return products.isEmpty
+        ? Scaffold(
+            body: CartEmpty(),
+          )
+        : Scaffold(
+            appBar: AppBar(
               title: Text('Cart Items Count'),
               actions: [
                 IconButton(
@@ -91,12 +21,8 @@ class Cart extends StatelessWidget {
                   icon: Icon(Icons.delete),
                 )
               ],
-            )
-          : null,
-      bottomSheet: products.isNotEmpty ? checkoutSection(context) : null,
-      body: products.isEmpty
-          ? CartEmpty()
-          : Container(
+            ),
+            body: Container(
               margin: const EdgeInsets.only(bottom: 60),
               child: ListView.builder(
                 itemCount: 5,
@@ -105,6 +31,6 @@ class Cart extends StatelessWidget {
                 },
               ),
             ),
-    );
+          );
   }
 }
