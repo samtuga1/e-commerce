e1b1f1eb85ae6c11a4d0ccf4e4aade86e67562a0
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-19 02:25:31 +0000

    update

---
+++ lib/widgets/wishlist_full.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import '../consts/colors.dart';
+
+class WishListItem extends StatefulWidget {
+  @override
+  _WishListItemState createState() => _WishListItemState();
+}
+
+class _WishListItemState extends State<WishListItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: double.infinity,
+          margin: EdgeInsets.only(right: 30.0, bottom: 10.0),
+          child: Material(
+            color: Theme.of(context).backgroundColor,
+            borderRadius: BorderRadius.circular(5.0),
+            elevation: 3.0,
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 80,
+                      child: Image.network(
+                          'https://abong.com.bd/public//admin/media/2020/03/yellow_mesh_men_sport_sneaker_shoesjpeg_20200307141459.jpeg'),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'title',
+                            style: TextStyle(
+                                fontSize: 16.0, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Text(
+                            "\$ 16",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        positionedRemove(),
+      ],
+    );
+  }
+
+  Widget positionedRemove() {
+    return Positioned(
+      top: 20,
+      right: 15,
+      child: Container(
+        height: 30,
+        width: 30,
+        child: MaterialButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          padding: EdgeInsets.all(0.0),
+          color: ColorsConsts.favColor,
+          child: Icon(
+            Icons.clear,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
