5e018d1da99f594869a025a48502187336338f5e
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-19 02:24:55 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:e_commerse/consts/theme_data.dart';
+import 'package:e_commerse/inner_screens/product_detail_page.dart';
 import 'package:e_commerse/models/dark_theme_sharedpreference.dart';
 import 'package:e_commerse/providers/theme_provider.dart';
+import 'package:e_commerse/screens/cart.dart';
 import 'package:e_commerse/screens/user_info.dart';
 import 'package:e_commerse/screens/wishlist.dart';
 import 'package:flutter/material.dart';
@@ -50,6 +52,8 @@ class _MyAppState extends State<MyApp> {
             BrandNavigationRailScreen.routeName: (context) =>
                 BrandNavigationRailScreen(),
             WishlistScreen.routeName: (context) => WishlistScreen(),
+            ProductDetailPage.routeName: (context) => ProductDetailPage(),
+            CartScreen.routeName: (context) => CartScreen(),
           },
         );
       }),
