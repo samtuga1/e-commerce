24cadbb6d33e0db85643ea2853016a7fa72f5a9b
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-19 12:20:48 +0000

    update

--- lib/widgets/category.dart
+++ lib/widgets/category.dart
@@ -1,4 +1,7 @@
+import 'package:e_commerse/inner_screens/category_feeds.dart';
 import 'package:flutter/material.dart';
+
+import '../screens/feeds.dart';
 
 class Category extends StatelessWidget {
   Category({Key? key, required this.index}) : super(key: key);
@@ -18,7 +21,7 @@ class Category extends StatelessWidget {
       'imagePath': 'assets/images/sneaker-category.jpg',
     },
     {
-      'name': 'Beauty&Health',
+      'name': 'Beauty & health',
       'imagePath': 'assets/images/beauty-category.jpg',
     },
     {
@@ -39,27 +42,33 @@ class Category extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          topRight: Radius.circular(10),
-        ),
-        child: SizedBox(
-          width: 150,
-          child: GridTile(
-            child: Image.asset(
-              categoryList[index]['imagePath'] ??
-                  'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640',
-              fit: BoxFit.cover,
-            ),
-            footer: GridTileBar(
-              backgroundColor: Theme.of(context).backgroundColor,
-              title: Text(
-                categoryList[index]['name']!,
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 18,
-                  color: Theme.of(context).textSelectionColor,
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).pushNamed(CategoryFeedsScreen.routeName,
+              arguments: categoryList[index]['name']);
+        },
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+          child: SizedBox(
+            width: 150,
+            child: GridTile(
+              child: Image.asset(
+                categoryList[index]['imagePath'] ??
+                    'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640',
+                fit: BoxFit.cover,
+              ),
+              footer: GridTileBar(
+                backgroundColor: Theme.of(context).backgroundColor,
+                title: Text(
+                  categoryList[index]['name']!,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                    color: Theme.of(context).textSelectionColor,
+                  ),
                 ),
               ),
             ),
