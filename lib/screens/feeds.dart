70c8166778509cefee8fdee29b66edf3ab389e3c
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-19 12:20:44 +0000

    update

--- lib/screens/feeds.dart
+++ lib/screens/feeds.dart
@@ -1,12 +1,19 @@
+import 'package:e_commerse/providers/product_provider.dart';
 import 'package:e_commerse/widgets/feeds_products.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:provider/provider.dart';
+
+import '../models/product_model.dart';
 
 class Feeds extends StatelessWidget {
-  const Feeds({Key? key}) : super(key: key);
+  static const routeName = '/feeds';
+  Feeds({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    List<Product> products = productsData.products;
     return Scaffold(
         body: SafeArea(
       child: MasonryGridView.count(
@@ -14,9 +21,12 @@ class Feeds extends StatelessWidget {
         crossAxisCount: 2,
         mainAxisSpacing: 4,
         crossAxisSpacing: 4,
-        itemCount: 10,
-        itemBuilder: (context, index) => FeedProducts(
-          height: index % 2 == 0 ? 310 : 270,
+        itemCount: products.length,
+        itemBuilder: (context, index) => ChangeNotifierProvider.value(
+          value: products[index],
+          child: FeedProducts(
+            height: index % 2 == 0 ? 310 : 350,
+          ),
         ),
       ),
     ));
