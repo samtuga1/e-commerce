edc4cf573c1008883505eef740e72267ff0d51a5
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-24 17:15:18 +0000

    update

---
+++ lib/services/global_methods.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class GlobalMethods {
+  Future<void> showDialogg(String title, String subtitle, Function() fxn,
+      BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 6),
+              child: Image.network(
+                'https://www.citypng.com/public/uploads/preview/yield-caution-red-triangle-caution-road-traffic-11585779316nnaqrpebpo.png',
+                height: 20,
+                width: 20,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(title),
+            ),
+          ],
+        ),
+        content: Text(subtitle),
+        actions: [
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancel'),
+          ),
+          FlatButton(
+            onPressed: () {
+              fxn();
+              Navigator.of(context).pop();
+            },
+            child: Text('Ok'),
+          ),
+        ],
+      ),
+    );
+  }
+}
