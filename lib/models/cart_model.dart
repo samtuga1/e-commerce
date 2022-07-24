b4adb81588a55b6dff01da8831d291dc5b16b24e
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-24 10:14:09 +0000

    update

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,9 @@
+class Cart {
+  final String? id;
+  final String? title;
+  final int? quantity;
+  final String? imageUrl;
+  final double? price;
+
+  Cart({this.id, this.title, this.quantity, this.imageUrl, this.price});
+}
