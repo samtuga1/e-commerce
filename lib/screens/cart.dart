40cb78ddbd7fafa32e931521bffb17fef152ae29
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-24 10:14:18 +0000

    update

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,19 +1,19 @@
-import 'package:e_commerse/widgets/cart_empty.dart';
-import 'package:e_commerse/widgets/cart_full.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../consts/colors.dart';
+import '../providers/carts_provider.dart';
+import '../widgets/cart_empty.dart';
+import '../widgets/cart_full.dart';
 
 class CartScreen extends StatelessWidget {
-  static const routeName = '/carts';
-  const CartScreen({Key? key}) : super(key: key);
-
+  static const routeName = '/CartScreen';
   @override
   Widget build(BuildContext context) {
-    List products = [2];
-    return products.isEmpty
-        ? Scaffold(
-            body: CartEmpty(),
-          )
+    final cartData = Provider.of<CartProvider>(context);
+    return cartData.cartItems.isEmpty
+        ? Scaffold(body: CartEmpty())
         : Scaffold(
+            bottomSheet: checkoutSection(context),
             appBar: AppBar(
               title: Text('Cart Items Count'),
               actions: [
@@ -24,14 +24,80 @@ class CartScreen extends StatelessWidget {
               ],
             ),
             body: Container(
-              margin: const EdgeInsets.only(bottom: 60),
+              margin: EdgeInsets.only(bottom: 60),
               child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (BuildContext context, int index) {
-                  return CartFull();
-                },
-              ),
+                  itemCount: 5,
+                  itemBuilder: (BuildContext ctx, int index) {
+                    return CartFull();
+                  }),
             ),
           );
+  }
+
+  Widget checkoutSection(BuildContext ctx) {
+    return Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(color: Colors.grey, width: 0.5),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            /// mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    gradient: LinearGradient(colors: [
+                      ColorsConsts.gradiendLStart,
+                      ColorsConsts.gradiendLEnd,
+                    ], stops: [
+                      0.0,
+                      0.7
+                    ]),
+                  ),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(30),
+                      onTap: () {},
+                      splashColor: Theme.of(ctx).splashColor,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Checkout',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Theme.of(ctx).textSelectionColor,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Spacer(),
+              Text(
+                'Total:',
+                style: TextStyle(
+                    color: Theme.of(ctx).textSelectionColor,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600),
+              ),
+              Text(
+                'US \$179.0',
+                //textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+        ));
   }
 }
