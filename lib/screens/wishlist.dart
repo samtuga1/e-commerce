491a6b9a0987811d3d736ab4c5b055b364c45689
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-24 21:20:40 +0000

    update

--- lib/screens/wishlist.dart
+++ lib/screens/wishlist.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerse/providers/wishlist_provider.dart';
+import 'package:e_commerse/services/global_methods.dart';
 import 'package:e_commerse/widgets/wishlist_empty.dart';
 import 'package:e_commerse/widgets/wishlist_full.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,7 @@ class WishlistScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final favsListData = Provider.of<WishListProvider>(context);
+    GlobalMethods globalMethods = GlobalMethods();
     return favsListData.favsLists.isEmpty
         ? Scaffold(
             body: WishlistEmpty(),
@@ -18,6 +20,23 @@ class WishlistScreen extends StatelessWidget {
         : Scaffold(
             appBar: AppBar(
               title: Text('Wishlists (${favsListData.favsLists.length})'),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    globalMethods.showDialogg(
+                      'Clear wishlist',
+                      'This will clear all your wishlists',
+                      () => favsListData.clearWishLists(),
+                      context,
+                    );
+                    //cartData.deletecartItem(widget.prodId);
+                  },
+                  icon: Icon(
+                    Icons.remove_outlined,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
             ),
             body: ListView.builder(
               itemCount: favsListData.favsLists.length,
