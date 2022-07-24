bf35c19b63654c30719777c21e1f5d1dc7a62e58
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-24 11:22:33 +0000

    update

--- lib/widgets/cart_full.dart
+++ lib/widgets/cart_full.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerse/inner_screens/product_detail_page.dart';
+import 'package:e_commerse/models/cart_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -5,6 +7,10 @@ import '../consts/colors.dart';
 import '../providers/theme_provider.dart';
 
 class CartFull extends StatefulWidget {
+  final String prodId;
+  CartFull({
+    required this.prodId,
+  });
   @override
   _CartFullState createState() => _CartFullState();
 }
@@ -13,167 +19,173 @@ class _CartFullState extends State<CartFull> {
   @override
   Widget build(BuildContext context) {
     final themeChange = Provider.of<DarkThemeProvider>(context);
-    return Container(
-      height: 135,
-      margin: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          bottomRight: const Radius.circular(16.0),
-          topRight: const Radius.circular(16.0),
+    final cartAttributes = Provider.of<Cart>(context);
+    print(cartAttributes.title);
+    return InkWell(
+      onTap: () => Navigator.of(context)
+          .pushNamed(ProductDetailPage.routeName, arguments: widget.prodId),
+      child: Container(
+        height: 135,
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            bottomRight: const Radius.circular(16.0),
+            topRight: const Radius.circular(16.0),
+          ),
+          color: Theme.of(context).backgroundColor,
         ),
-        color: Theme.of(context).backgroundColor,
-      ),
-      child: Row(
-        children: [
-          Container(
-            width: 130,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4PdHtXka2-bDDww6Nuect3Mt9IwpE4v4HNw&usqp=CAU'),
-                fit: BoxFit.fill,
+        child: Row(
+          children: [
+            Container(
+              width: 130,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    cartAttributes.imageUrl!,
+                  ),
+                  fit: BoxFit.fill,
+                ),
               ),
             ),
-          ),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        child: Text(
-                          'title',
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              fontWeight: FontWeight.w600, fontSize: 15),
-                        ),
-                      ),
-                      Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(32.0),
-                          // splashColor: ,
-                          onTap: () {},
-                          child: Container(
-                            height: 50,
-                            width: 50,
-                            child: Icon(
-                              Icons.delete,
-                              color: Colors.red,
-                              size: 22,
-                            ),
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          child: Text(
+                            cartAttributes.title!,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 15),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text('Price:'),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        '450\$',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w600),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text('Sub Total:'),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        '450\$',
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            color: themeChange.darkTheme
-                                ? Colors.brown.shade900
-                                : Theme.of(context).accentColor),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'Ships Free',
-                        style: TextStyle(
-                            color: themeChange.darkTheme
-                                ? Colors.brown.shade900
-                                : Theme.of(context).accentColor),
-                      ),
-                      Spacer(),
-                      Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(4.0),
-                          // splashColor: ,
-                          onTap: () {},
-                          child: Container(
-                            child: Padding(
-                              padding: const EdgeInsets.all(5.0),
+                        Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(32.0),
+                            // splashColor: ,
+                            onTap: () {},
+                            child: Container(
+                              height: 50,
+                              width: 50,
                               child: Icon(
-                                Icons.remove,
+                                Icons.delete,
                                 color: Colors.red,
                                 size: 22,
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      Card(
-                        elevation: 12,
-                        child: Container(
-                          width: MediaQuery.of(context).size.width * 0.12,
-                          padding: const EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(colors: [
-                              ColorsConsts.gradiendLStart,
-                              ColorsConsts.gradiendLEnd,
-                            ], stops: [
-                              0.0,
-                              0.7
-                            ]),
-                          ),
-                          child: Text(
-                            '154',
-                            textAlign: TextAlign.center,
-                          ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('Price: '),
+                        SizedBox(
+                          width: 5,
                         ),
-                      ),
-                      Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(4.0),
-                          // splashColor: ,
-                          onTap: () {},
-                          child: Container(
-                            child: Padding(
-                              padding: const EdgeInsets.all(5.0),
-                              child: Icon(
-                                Icons.add,
-                                color: Colors.green,
-                                size: 22,
+                        Text(
+                          '${cartAttributes.price}\$',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('Sub Total:'),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          '${cartAttributes.price! * cartAttributes.quantity!}\$',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: themeChange.darkTheme
+                                  ? Colors.brown.shade900
+                                  : Theme.of(context).accentColor),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'Ships Free',
+                          style: TextStyle(
+                              color: themeChange.darkTheme
+                                  ? Colors.brown.shade900
+                                  : Theme.of(context).accentColor),
+                        ),
+                        Spacer(),
+                        Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(4.0),
+                            // splashColor: ,
+                            onTap: () {},
+                            child: Container(
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Icon(
+                                  Icons.remove,
+                                  color: Colors.red,
+                                  size: 22,
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                        Card(
+                          elevation: 12,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.12,
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(colors: [
+                                ColorsConsts.gradiendLStart,
+                                ColorsConsts.gradiendLEnd,
+                              ], stops: [
+                                0.0,
+                                0.7
+                              ]),
+                            ),
+                            child: Text(
+                              cartAttributes.quantity.toString(),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                        Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(4.0),
+                            onTap: () {},
+                            child: Container(
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Icon(
+                                  Icons.add,
+                                  color: Colors.green,
+                                  size: 22,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
