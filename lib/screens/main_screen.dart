0dd13082a9079af948d65628831903e7efae14c3
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-09 16:46:20 +0000

    update

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,19 @@
+import 'package:e_commerse/inner_screens/upload_product_form.dart';
+import 'package:flutter/material.dart';
+
+import 'bottom_bar.dart';
+
+class MainScreen extends StatelessWidget {
+  static const routeName = 'main_page';
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        BottomBarScreen(),
+        UploadProductForm(),
+      ],
+    );
+  }
+}
