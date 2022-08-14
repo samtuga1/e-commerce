fd9b1062d5bdd52e9e38a09a1acffe51004f8349
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-14 22:57:50 +0000

    update

--- lib/services/global_methods.dart
+++ lib/services/global_methods.dart
@@ -42,6 +42,46 @@ class GlobalMethods {
     );
   }
 
+  Future<void> showLogoutDialog(Function() fxn, BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 6),
+              child: Image.network(
+                'https://www.citypng.com/public/uploads/preview/yield-caution-red-triangle-caution-road-traffic-11585779316nnaqrpebpo.png',
+                height: 20,
+                width: 20,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Signout'),
+            ),
+          ],
+        ),
+        content: Text('Do you really wanna Sign out?'),
+        actions: [
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancel'),
+          ),
+          FlatButton(
+            onPressed: () {
+              fxn();
+              Navigator.of(context).pop();
+            },
+            child: Text('Yes'),
+          ),
+        ],
+      ),
+    );
+  }
+
   Future<void> authErrorHandle(String subtitle, BuildContext context) async {
     await showDialog(
       context: context,
