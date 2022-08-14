1a1713fb1b8d2c53ed1f773545e727514e8c63e0
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-14 23:16:17 +0000

    update

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -3,9 +3,12 @@ import 'package:e_commerse/screens/auth/login.dart';
 import 'package:e_commerse/screens/auth/sign_up.dart';
 import 'package:e_commerse/screens/bottom_bar.dart';
 import 'package:e_commerse/screens/main_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../consts/colors.dart';
+import '../services/global_methods.dart';
 
 class LandingPage extends StatefulWidget {
   @override
@@ -16,6 +19,8 @@ class _LandingPageState extends State<LandingPage>
     with TickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<double> _animation;
+  GlobalMethods _globalMethods = GlobalMethods();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   List<String> images = [
     'https://media.istockphoto.com/photos/man-at-the-shopping-picture-id868718238?k=6&m=868718238&s=612x612&w=0&h=ZUPCx8Us3fGhnSOlecWIZ68y3H4rCiTnANtnjHk0bvo=',
     'https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F1138257321%2F0x0.jpg%3Ffit%3Dscale',
@@ -46,6 +51,21 @@ class _LandingPageState extends State<LandingPage>
   void dispose() {
     _animationController.dispose();
     super.dispose();
+  }
+
+  Future<UserCredential> googleSignIn() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   @override
@@ -213,7 +233,7 @@ class _LandingPageState extends State<LandingPage>
                     ),
                   ),
                 ),
-                onPressed: () {},
+                onPressed: googleSignIn,
                 child: Text('Google +'),
               ),
               OutlinedButton(
