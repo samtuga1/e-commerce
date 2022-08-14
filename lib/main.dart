ad68a261dbe9605366c44103e9846a3c10ac2657
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-14 20:24:02 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:developer';
+
 import 'package:e_commerse/consts/theme_data.dart';
+import 'package:e_commerse/firebase_options.dart';
 import 'package:e_commerse/inner_screens/product_detail_page.dart';
-import 'package:e_commerse/models/dark_theme_sharedpreference.dart';
 import 'package:e_commerse/providers/carts_provider.dart';
 import 'package:e_commerse/providers/product_provider.dart';
 import 'package:e_commerse/providers/theme_provider.dart';
@@ -13,14 +15,15 @@ import 'package:e_commerse/screens/landing_page.dart';
 import 'package:e_commerse/screens/main_screen.dart';
 import 'package:e_commerse/screens/user_info.dart';
 import 'package:e_commerse/screens/wishlist.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'inner_screens/brands_navigation_rail copy.dart';
 import 'inner_screens/category_feeds.dart';
 import 'screens/bottom_bar.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -43,44 +46,74 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
+  final initialize = Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => _themeProvider,
-        ),
-        ChangeNotifierProvider(
-          create: (context) => Products(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => CartProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => WishListProvider(),
-        ),
-      ],
-      child: Consumer<DarkThemeProvider>(builder: (context, themeData, child) {
-        return MaterialApp(
-          title: 'Flutter Demo',
-          theme: Styles.themeData(themeData.darkTheme, context),
-          home: LandingPage(),
-          routes: {
-            UserInfo.routeName: (context) => UserInfo(),
-            BrandNavigationRailScreen.routeName: (context) =>
-                BrandNavigationRailScreen(),
-            WishlistScreen.routeName: (context) => WishlistScreen(),
-            ProductDetailPage.routeName: (context) => ProductDetailPage(),
-            CartScreen.routeName: (context) => CartScreen(),
-            Feeds.routeName: (context) => Feeds(),
-            CategoryFeedsScreen.routeName: (context) => CategoryFeedsScreen(),
-            LoginScreen.routeName: (context) => LoginScreen(),
-            SignUpScreen.routeName: (context) => SignUpScreen(),
-            BottomBarScreen.routeName: (context) => BottomBarScreen(),
-            MainScreen.routeName: (context) => MainScreen(),
-          },
-        );
-      }),
+    return FutureBuilder(
+      future: initialize,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          log(snapshot.error.toString());
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text('Firebase Initialize Error'),
+              ),
+            ),
+          );
+        } else {
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(
+                create: (_) => _themeProvider,
+              ),
+              ChangeNotifierProvider(
+                create: (context) => Products(),
+              ),
+              ChangeNotifierProvider(
+                create: (context) => CartProvider(),
+              ),
+              ChangeNotifierProvider(
+                create: (ctx) => WishListProvider(),
+              ),
+            ],
+            child: Consumer<DarkThemeProvider>(
+                builder: (context, themeData, child) {
+              return MaterialApp(
+                title: 'Flutter Demo',
+                theme: Styles.themeData(themeData.darkTheme, context),
+                home: LandingPage(),
+                routes: {
+                  UserInfo.routeName: (context) => UserInfo(),
+                  BrandNavigationRailScreen.routeName: (context) =>
+                      BrandNavigationRailScreen(),
+                  WishlistScreen.routeName: (context) => WishlistScreen(),
+                  ProductDetailPage.routeName: (context) => ProductDetailPage(),
+                  CartScreen.routeName: (context) => CartScreen(),
+                  Feeds.routeName: (context) => Feeds(),
+                  CategoryFeedsScreen.routeName: (context) =>
+                      CategoryFeedsScreen(),
+                  LoginScreen.routeName: (context) => LoginScreen(),
+                  SignUpScreen.routeName: (context) => SignUpScreen(),
+                  BottomBarScreen.routeName: (context) => BottomBarScreen(),
+                  MainScreen.routeName: (context) => MainScreen(),
+                },
+              );
+            }),
+          );
+        }
+      },
     );
   }
 }
