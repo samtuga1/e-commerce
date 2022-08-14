d88e87b6ff3cb9e394de1ce48030b7aae667e593
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-14 21:15:08 +0000

    update

--- lib/services/global_methods.dart
+++ lib/services/global_methods.dart
@@ -41,4 +41,37 @@ class GlobalMethods {
       ),
     );
   }
+
+  Future<void> authErrorHandle(String subtitle, BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 6),
+              child: Image.network(
+                'https://www.citypng.com/public/uploads/preview/yield-caution-red-triangle-caution-road-traffic-11585779316nnaqrpebpo.png',
+                height: 20,
+                width: 20,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Error'),
+            ),
+          ],
+        ),
+        content: Text(subtitle),
+        actions: [
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Ok'),
+          ),
+        ],
+      ),
+    );
+  }
 }
