cd7a9746e95b7bb8e6c72c6ba9f2f81378804c3c
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-08-14 22:06:09 +0000

    update

---
+++ lib/screens/user_state.dart
@@ -0,0 +1,32 @@
+import 'package:e_commerse/screens/landing_page.dart';
+import 'package:e_commerse/screens/main_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class UserState extends StatelessWidget {
+  const UserState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, AsyncSnapshot userSnapshot) {
+        if (userSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (userSnapshot.connectionState == ConnectionState.active) {
+          if (userSnapshot.hasData) {
+            return MainScreen();
+          } else {
+            return LandingPage();
+          }
+        } else {
+          return Center(
+            child: Text('Error occured'),
+          );
+        }
+      },
+    );
+  }
+}
