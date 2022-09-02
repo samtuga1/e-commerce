15141e77273ae60b8565eff29b8986ad9940a2fb
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-09-02 16:10:50 +0000

    update

--- lib/widgets/wishlist_empty.dart
+++ lib/widgets/wishlist_empty.dart
@@ -8,68 +8,70 @@ class WishlistEmpty extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final themeChange = Provider.of<DarkThemeProvider>(context);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          margin: EdgeInsets.only(top: 80),
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height * 0.4,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage('assets/images/emptycart.png'),
-            ),
-          ),
-        ),
-        Text(
-          'Your WishList is empty',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              color: Theme.of(context).textSelectionColor,
-              fontSize: 36,
-              fontWeight: FontWeight.w600),
-        ),
-        SizedBox(
-          height: 30,
-        ),
-        Text(
-          'You need to explore more',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              color: themeChange.darkTheme
-                  ? Theme.of(context).disabledColor
-                  : ColorsConsts.subTitle,
-              fontSize: 26,
-              fontWeight: FontWeight.w600),
-        ),
-        SizedBox(
-          height: 30,
-        ),
-        Container(
-          width: MediaQuery.of(context).size.width * 0.9,
-          height: MediaQuery.of(context).size.height * 0.06,
-          child: RaisedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(Feeds.routeName);
-            },
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-              side: BorderSide(color: Colors.red),
-            ),
-            color: Colors.redAccent,
-            child: Text(
-              'Add a wish'.toUpperCase(),
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Theme.of(context).textSelectionColor,
-                fontSize: 26,
-                fontWeight: FontWeight.w600,
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 80),
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.4,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage('assets/images/emptycart.png'),
               ),
             ),
           ),
-        ),
-      ],
+          Text(
+            'Your WishList is empty',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Theme.of(context).textSelectionColor,
+                fontSize: 36,
+                fontWeight: FontWeight.w600),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'You need to explore more',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: themeChange.darkTheme
+                    ? Theme.of(context).disabledColor
+                    : ColorsConsts.subTitle,
+                fontSize: 26,
+                fontWeight: FontWeight.w600),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: MediaQuery.of(context).size.height * 0.06,
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(Feeds.routeName);
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+                side: BorderSide(color: Colors.red),
+              ),
+              color: Colors.redAccent,
+              child: Text(
+                'Add a wish'.toUpperCase(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Theme.of(context).textSelectionColor,
+                  fontSize: 26,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
