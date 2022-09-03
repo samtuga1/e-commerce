8880b53feff2c356a35cb23116697547bf3b7acd
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-09-03 13:22:32 +0000

    update

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -2,10 +2,18 @@ import 'package:flutter/cupertino.dart';
 
 class Cart with ChangeNotifier {
   final String? id;
+  final String productId;
   final String? title;
   final int? quantity;
   final String? imageUrl;
   final double? price;
 
-  Cart({this.id, this.title, this.quantity, this.imageUrl, this.price});
+  Cart({
+    required this.productId,
+    this.id,
+    this.title,
+    this.quantity,
+    this.imageUrl,
+    this.price,
+  });
 }
